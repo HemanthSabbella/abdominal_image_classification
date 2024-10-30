ef18382526d512e22c777ade0e04e0f2675d8f56
Mingyuan <mingyuanwang1990@gmail.com>
2024-10-30 14:23:45 +0800

    Update sbatchTemplatePython.sh

--- sbatchTemplatePython.sh
+++ sbatchTemplatePython.sh
@@ -9,8 +9,8 @@
 
 #SBATCH --nodes=1                   # How many nodes required? Usually 1
 #SBATCH --cpus-per-task=4           # Number of CPU to request for the job
-#SBATCH --mem=8GB                   # How much memory does your job require?
-#SBATCH --gres=gpu:1                # Do you require GPUS? If not delete this line
+#SBATCH --mem=16GB                   # How much memory does your job require?
+#SBATCH --gres=gpu:4                # Do you require GPUS? If not delete this line
 #SBATCH --time=24:00:00        # How long to run the job for? Jobs exceed this time will be terminated
                                     # Format <DD-HH:MM:SS> eg. 5 days 05-00:00:00
                                     # Format <DD-HH:MM:SS> eg. 24 hours 1-00:00:00 or 24:00:00
@@ -18,6 +18,7 @@
 #SBATCH --output=%u.%j.out          # Where should the log files go?
                                     # You must provide an absolute path eg /common/home/module/username/
                                     # If no paths are provided, the output file will be placed in your current working directory
+#SBATCH --constraint="a100|v100|a40"  # Preference for A100, fallback to V100 or A40
 
 ################################################################
 ## EDIT AFTER THIS LINE IF YOU ARE OKAY WITH DEFAULT SETTINGS ##
@@ -33,6 +34,7 @@
 ##            END OF SBATCH COMMANDS           ##
 #################################################
 
+
 # Purge the environment, load the modules we require.
 # Refer to https://violet.smu.edu.sg/origami/module/ for more information
 module purge
@@ -45,11 +47,15 @@ python3 -m venv ~/myenv
 # We're using an absolute path here. You may use a relative path, as long as SRUN is execute in the same working directory
 source ~/myenv/bin/activate
 
+# Find out which GPU you are using
+srun whichgpu
+
 # If you require any packages, install it as usual before the srun job submission.
 pip3 install numpy
 pip3 install torch
 pip3 install scipy
 pip3 install pillow
+pip install scikit-image
 
 # Submit your job to the cluster
 srun --gres=gpu:1 python3 /common/home/projectgrps/CS701/CS701G8/gitcodes/CS701_G8/main.py

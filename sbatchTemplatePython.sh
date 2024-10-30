af64b28763f1d2e4ae3acfbb87d8e090c5dde6f8
Mingyuan <mingyuanwang1990@gmail.com>
2024-10-30 14:27:25 +0800

    Update sbatchTemplatePython.sh

--- sbatchTemplatePython.sh
+++ sbatchTemplatePython.sh
@@ -10,7 +10,7 @@
 #SBATCH --nodes=1                   # How many nodes required? Usually 1
 #SBATCH --cpus-per-task=4           # Number of CPU to request for the job
 #SBATCH --mem=16GB                   # How much memory does your job require?
-#SBATCH --gres=gpu:4                # Do you require GPUS? If not delete this line
+#SBATCH --gres=gpu:1                # Do you require GPUS? If not delete this line
 #SBATCH --time=24:00:00        # How long to run the job for? Jobs exceed this time will be terminated
                                     # Format <DD-HH:MM:SS> eg. 5 days 05-00:00:00
                                     # Format <DD-HH:MM:SS> eg. 24 hours 1-00:00:00 or 24:00:00
